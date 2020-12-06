6f73e048342285ee1bf6b6402bc4ca9b50cbda16
noah-green <ngreen612@gmail.com>
2020-12-06 00:35:23 -0500

    day 6, made solutions more symmetrical

--- day6.py
+++ day6.py
@@ -1,10 +1,8 @@
 puzzleName = 'Custom Customs'
 
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-
 def solvePart1(puzzleInput):
-    return sum(len(set(''.join(group.split()))) for group in puzzleInput.split('\n\n'))
+    return sum(len(set().union(*group.split())) for group in puzzleInput.split('\n\n'))
 
 
 def solvePart2(puzzleInput):
-    return sum(len(set(alpha).intersection(*group.split())) for group in puzzleInput.split('\n\n'))
+    return sum(len(set('abcdefghijklmnopqrstuvwxyz').intersection(*group.split())) for group in puzzleInput.split('\n\n'))
